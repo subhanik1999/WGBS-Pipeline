64c6187b0ddf177da414e2e1465314820ca76be3
Subhanik Purkayastha <47398273+subhanik1999@users.noreply.github.com>
2019-08-15 13:40:23 -0700

    Update numReads.sh

--- numReads.sh
+++ numReads.sh
@@ -2,12 +2,13 @@
 
 PREFIX=$SGE_TASK_ID
 
-#touch "readsInBam${PREFIX}.txt"
+# get total number of reads from fastq file i.e. lines divided by 4
 reads=$(cat ./$PREFIX/$PREFIX.R1.fq|wc -l)
 reads=$((reads / 4))
 echo "fastq reads "
 echo $reads >> "reads${PREFIX}.txt"
 
+# get aligned reads in bam file
 reads=$(samtools view -c -F 4 $PREFIX.out.bam)
 echo "full bam "
 echo $reads >> "reads${PREFIX}.txt"
